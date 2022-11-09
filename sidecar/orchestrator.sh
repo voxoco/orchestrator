5ff8400c209ae0753515e455001e30e074969170
Joe Mordica <jmordica@gmail.com>
2022-11-09 16:13:01 -0600

    added re-election in case follower is unhealthy in raft cluster

--- sidecar/orchestrator.sh
+++ sidecar/orchestrator.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# LAST_RAFT_CHECK_TIME is the last time we checked raft follower status
+LAST_RAFT_CHECK_TIME=$(date +%s)
+
 exit_script() {
   echo "Tearing down..."
   trap - SIGINT SIGTERM # clear the trap
@@ -59,10 +62,40 @@ bootstrap() {
   done
 }
 
+check_healthy_raft() {
+  echo "Checking if raft is healthy"
+
+  # Orchestrator api becomes unresponsive it seems if time has passed and 'request-health-report' returns 500
+  # So we need to check first if we are a follower and if so, check the 'health' endpoint of the leader
+  # to make sure our IP is in the array of 'RaftHealthyMembers'
+  # If not, we need to call the 'reelect' enpoint to force a new leader election (so the token is updated)
+
+  # Check if we are a follower
+  RAFT_STATUS=$(curl -m 1 -s http://127.0.0.1:3000/api/raft-status)
+  if [ "$(echo $RAFT_STATUS | jq -r .State)" != "Follower" ]; then return; fi
+  
+  # Get the leader
+  LEADER=$(echo $RAFT_STATUS | jq -r .Leader | sed 's/:.*//')
+  echo "Checking if follower $PODIP is healthy in the raft cluster via leader: $LEADER"
+
+  # Check to make sure our IP is in the array of 'RaftHealthyMembers' of the Leader
+  if curl -m 1 -s http://$LEADER:3000/api/health | jq -r '.Details.RaftHealthyMembers[]' | grep -q $PODIP; then return; fi
+  
+  echo "Our IP is not in the array of 'RaftHealthyMembers' of the Leader, forcing a new leader election"
+  curl -m 1 -s http://$LEADER:3000/api/reelect
+}
+
 # catch kill signals
 trap exit_script SIGINT SIGTERM
 
 # Bootstrap the cluster
 bootstrap
 
-while true; do sleep 1; done;
+while true; do
+  sleep 1
+  # Check raft follower status every minute
+  if [ $(($(date +%s) - $LAST_RAFT_CHECK_TIME)) -gt 60 ]; then
+    check_healthy_raft
+    LAST_RAFT_CHECK_TIME=$(date +%s)
+  fi
+done
